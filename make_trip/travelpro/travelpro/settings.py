876789dbcfbbd9f3d0298883d44da0dc2712007b
Keyur-Destiny <keyur.patel@thedestinysolutions.com>
2023-10-26 18:42:10 +0530

    change in file

--- make_trip/travelpro/travelpro/settings.py
+++ make_trip/travelpro/travelpro/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('static',)
+STATIC_ROOT = os.path.join(BASE_DIR,"static/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -133,3 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URl = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR)
+
+
+
